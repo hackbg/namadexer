056851d26570935663f1d64fbd64944e11945ed0
Adam A <adam@hack.bg>
2024-02-13 15:21:26 +0200

    chore: more elaborate shell.nix

--- shell.nix
+++ shell.nix
@@ -1,1 +1,13 @@
-import (builtins.fetchurl https://raw.githubusercontent.com/hackbg/arsenal/main/shells/rust-protobuf.nix)
+let
+  rev   = "a5da8f82aadf0b90175eace3ed96319c6dd9d5dd";
+  url   = "https://raw.githubusercontent.com/hackbg/arsenal/${rev}/shells/rust-protobuf.nix";
+  pkgs  = builtins.fetchGit {
+    url = "https://github.com/hackbg/arsenal";
+    ref = "main";
+    rev = rev;
+  };
+in import (builtins.fetchurl url) {
+  pkgs = import <nixpkgs> {
+    overlays = import "${pkgs.outPath}/overlays.nix";
+  };
+}
